e617ff623f0890f050c7af2306c7a8baa1639fa5
nsinha <navendusinha@icloud.com>
2017-08-22 15:27:44 -0700

    Added more code-generators

--- StappedUI/UIApp.py
+++ StappedUI/UIApp.py
@@ -4,6 +4,7 @@ Created on Aug 21, 2017
 @author: nsinha
 '''
 from UIElements import JSFile, JSFunction
+import os
 
 
 class UIApp(object):
@@ -11,22 +12,52 @@ class UIApp(object):
     classdocs
     '''
 
-    _jsFile = None
+    _jsFiles = {}
     _html = None
 
-    def __init__(self, appName):
+    staticDir = None
+
+    def __init__(self, appName, staticDir):
         '''
         Constructor
         '''
-        self._jsFile = JSFile('app')
+        self.addAppJS()
 
-        func = self._jsFile.addJSFunc(name="appInit", params=None)
+    def addAppJS(self):
+        """
+        Adds app JS file
+        """
+
+        self._jsFiles['app'] = JSFile('app')
+
+        func = self._jsFiles['app'].addJSFunc(name="appInit", params=None)
         func.addConsoleLog("\"Hello word..\"")
 
-        func = self._jsFile.addJSFunc(name="loginView", params=None)
+        func = self._jsFiles['app'].addJSFunc(name="loginView", params=None)
+        func.addConsoleLog("\"Login View controller\"")
 
-        print "==> %s" % (self._jsFile.toString())
+    def addView(self, viewname):
+        """
+        Adds an app View
+        """
+
+    def writeJS(self):
+        """
+        Write JS files
+        """
+        jsdir = os.path.join(self.staticDir, 'js')
+        os.makedirs(jsdir)
+        for jfname, jfile in self._jsFiles.iteritems():
+            fw = open(os.path.join(jsdir, jfname + ".js"), 'w')
+            fw.write(jfile.toString())
+
+    def write(self):
+        """
+        """
+        self.writeJS()
 
 
 if __name__ == "__main__":
-    uiapp = UIApp("Test App")
+    uiapp = UIApp(appName="Test App",
+                  staticDir="/Users/nsinha/git/BootstrapUI/www")
+    uiapp.write()
--- framework/js/app.js
+++ framework/js/app.js
@@ -26,7 +26,7 @@ function loadLandingView() {
 		
 		
 		ui.addSubViewToMain([jum]);
-	}
+}
 
 	
 
